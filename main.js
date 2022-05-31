7eed924015ca6f2e64df1fd0773bf980c21b8ccb
MaDKnight404 <battleaxe@bk.ru>
2022-06-01 00:32:38 +0300

    refactoring btn eventListeners

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 // buttons
 
-const sessionDown = document.querySelector('.session__down'),
-      sessionUp = document.querySelector('.session__up'),
-      breakDown = document.querySelector('.break__down'),
-      breakUp = document.querySelector('.break__up');
+const btnLength = document.querySelectorAll('.btn__length')
       sessionLenght = document.querySelector('.session__text__minutes');
       breakLenght = document.querySelector('.break__text__minutes');
 
@@ -13,29 +10,38 @@ const start = document.querySelector('.start'),
 
 // Listeners
 
-sessionDown.addEventListener('click', ()=> {
-    if (sessionLenght.textContent > 0) {
-        sessionLenght.textContent = +sessionLenght.textContent - 5;
-    }
+
+btnLength.forEach (btn => {
+    btn.addEventListener('click', (event) => {
+
+        if (event.target.getAttribute('id') == 'session__down') {
+        if (sessionLenght.textContent > 0) {
+            sessionLenght.textContent = +sessionLenght.textContent - 5;
+            }
+        }
+
+        if (event.target.getAttribute('id') == 'session__up') {
+            if (sessionLenght.textContent < 60) {
+                sessionLenght.textContent = +sessionLenght.textContent + 5;
+            }
+        }
+
+        if (event.target.getAttribute('id') == 'break__down') {
+            if (breakLenght.textContent > 0) {
+                breakLenght.textContent = +breakLenght.textContent - 5;
+            }
+        }
+
+        if (event.target.getAttribute('id') == 'break__up') {
+            if (breakLenght.textContent < 20) {
+                breakLenght.textContent = +breakLenght.textContent + 5;
+            }
+        }
+
+    })
 })
 
-sessionUp.addEventListener('click', ()=> {
-    if (sessionLenght.textContent < 60) {
-        sessionLenght.textContent = +sessionLenght.textContent + 5;
-    }
-})
 
-breakDown.addEventListener('click', ()=> {
-    if (breakLenght.textContent > 0) {
-        breakLenght.textContent = +breakLenght.textContent - 5;
-    }
-})
-
-breakUp.addEventListener('click', ()=> {
-    if (breakLenght.textContent < 20) {
-        breakLenght.textContent = +breakLenght.textContent + 5;
-    }
-})
 
 
 
